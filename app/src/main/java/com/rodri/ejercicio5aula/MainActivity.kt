d4de1dab0be4e12ef287d73e2202b61bada68df0
Rodrigo <rodsan946@alu.centroafuera.es>
2022-11-05 17:31:18 +0100

    Initial Commit

---
+++ app/src/main/java/com/rodri/ejercicio5aula/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rodri.ejercicio5aula
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
